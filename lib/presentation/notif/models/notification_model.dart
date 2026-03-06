414ef305ec0f69cfa20437fbbc0bb76c6f50a391
Mfajrialfaini <alvenlover@gmail.com>
2026-03-06 09:13:28 +0700

    06/03/2026 Memperbaiki ui di bagian riwayat dan recap

--- lib/presentation/notif/models/notification_model.dart
+++ lib/presentation/notif/models/notification_model.dart
@@ -3,7 +3,7 @@ class NotificationModel {
   final String body;
   final String time;
   final int badgeCount; // Untuk angka '2' di logo
-
+  // COBAAAA
   NotificationModel({
     required this.title,
     required this.body,
